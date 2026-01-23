feefb09e9049891c47af47df3f29c39594dbd260
Lukas Espejo <lucas@lcasesorias.cl>
2026-01-23 13:55:12 -0300

    initial feat empresa y usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AgendarComponent } from './pages/agendar/agendar';
+import { Usuario } from './pages/usuario/usuario';
+import { Empresa } from './pages/empresa/empresa';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'agendar', pathMatch: 'full' },
   { path: 'agendar', component: AgendarComponent },
+  { path: 'usuario', component: Usuario },
+  { path: 'empresa', component: Empresa },
 ];
 
---
+++ src/app/pages/empresa/empresa.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Usuario } from '../usuario/usuario';
+
+@Component({
+  selector: 'app-empresa',
+  imports: [Usuario],
+  templateUrl: './empresa.html',
+  styleUrl: './empresa.css',
+})
+export class Empresa {
+
+}
